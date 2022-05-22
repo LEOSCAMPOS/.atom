35f3f45bc266eeab0865dabb050fa304b391e604
LEOSCAMPOS <nadinholsc@gmail.com>
2022-05-22 15:14:43 -0300

    tentativa de resolução do problema cocm o grafico

--- reavaliacao_2022/exercícios/python_work/juros_compostos.py
+++ reavaliacao_2022/exercícios/python_work/juros_compostos.py
@@ -1,10 +1,16 @@
+
+investimento_inicial = float(input("Quanto você possui para o investimento inicial? "))
+rendimento_periodo = 0.54
+print(f"A projeção de rendimento para os períodos é de {rendimento_periodo}%")
+aporte_periodo = float(input("De quanto será o aporte nos períodos?: "))
+meses = int(input("Quantos meses de inventimento você fará? "))
+
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as grafico
 
 def valor_mais_redimento(valor, percentual):
     return valor + (valor * percentual / 100)
-
 
 def mostra_grafico(periodos, acumulados):
     grafico.plot(periodos, acumulados)
@@ -28,12 +34,5 @@ def projecao(inicio, rendimento, aporte, periodos):
             print("Após ", meses + 1, " períodos(s), o montante será de R$", round(acumulado, 2), ".")
 
         mostra_grafico(lista_periodos, lista_acumulados)
-
-
-investimento_inicial = float(input("Quanto você possui para o investimento inicial? "))
-rendimento_periodo = 0.54
-print(f"A projeção de rendimento para os períodos é de {rendimento_periodo}%")
-aporte_periodo = float(input("De quanto será o aporte nos períodos?: "))
-meses = int(input("Quantos meses de inventimento você fará? "))
 
 projecao(investimento_inicial, rendimento_periodo, aporte_periodo, meses)
